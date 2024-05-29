ad1f1e2774aed92ec4a1e3c49c2318ae54e374d3
AikoHikichi <aiko.hikichi@ctc-g.co.jp>
2024-05-29 07:22:00 +0000

    Added s3 bucket module

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,7 +13,17 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to ${PREFIX}'s module "s3-bucket" {
+  source              = "cloudposse/s3-bucket/aws"
+  version             = "3.1.0"
+  s3_object_ownership = "BucketOwnerEnforced"
+}cd /root/hcp-terraform
+
+# 1. VCSからポリシーセットを作成
+# 2. 自動化されたPolicy as Codeを有効化
+# 3. hashicat-awsのワークスペースに適用
+terraform apply -auto-approve
+app. Replace this text with your own.
   <!-- END -->
 
   </div>
